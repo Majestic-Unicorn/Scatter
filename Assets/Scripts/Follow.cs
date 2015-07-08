4b00bf00781326d3fdc44dac75c2b4b2c80eca53
Elliott Slingsby <elliottslingsby@googlemail.com>
2015-07-08 07:17:40 +0100

    Somewhat functioning mulitcam

--- Assets/Scripts/Follow.cs
+++ Assets/Scripts/Follow.cs
@@ -1,14 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(Camera))]
 public class Follow : MonoBehaviour {
+    public Camera camera;
     public Transform[] targets;
 
     public float relativeYaw = 0;
     public float relativePitch = 25;
 
-    public float height = 100;
+    private float height = 100;
+    private Vector3 position;
 
+    public float zoomInZone = 1f;
+    public float zoomOutZone = 1f;
+
+    public float zoomSpeed = 1f;
     public float ease = 0.075f;
 
     void Start() {
@@ -19,9 +26,13 @@ public class Follow : MonoBehaviour {
         transform.position = movement;
 
         transform.LookAt(average);
+
+        camera = GetComponent<Camera>();
+
+        position = transform.position;
     }
 
-    Vector3 RelativeRotation(Vector3 axis) {
+    private Vector3 RelativeRotation(Vector3 axis) {
         axis = Quaternion.AngleAxis(relativeYaw, Vector3.up) * axis;
 
         Vector3 direction = new Vector3(0, height, 0);
@@ -31,25 +42,79 @@ public class Follow : MonoBehaviour {
         return direction + axis;
     }
 
-    Vector3 AverageTarget() {
+    private Vector3 AverageTarget() {
+        Vector3 min = new Vector3(0, 0, 0);
+        Vector3 max = new Vector3(0, 0, 0);
+
         Vector3 average = new Vector3(0, 0, 0);
 
-        for (int i = 0; i < targets.Length; i++)
-        {
+        for (int i = 0; i < targets.Length; i++){
+            if (targets[i].position.x < min.x)
+                min.x = targets[i].position.x;
+            else if (targets[i].position.x > max.x)
+                max.x = targets[i].position.x;
+
+            if (targets[i].position.y < min.y)
+                min.y = targets[i].position.y;
+            else if (targets[i].position.y > max.y)
+                max.y = targets[i].position.y;
+
+            if (targets[i].position.z < min.z)
+                min.z = targets[i].position.z;
+            else if (targets[i].position.z > max.z)
+                max.z = targets[i].position.z;
+            
             average += targets[i].position;
         }
 
-        return average / targets.Length;
+        return (max + min) / 2;
+
+        //return average / targets.Length;
+    }
+
+    private bool inScreen(Vector3 target, float reduction) {
+        Vector3 screenPos = camera.WorldToScreenPoint(target);
+
+        if (screenPos.x < 0 + camera.pixelWidth * reduction)
+            return false;
+
+        if (screenPos.y < 0 + camera.pixelHeight * reduction)
+            return false;
+
+        if (screenPos.x > camera.pixelWidth - camera.pixelWidth * reduction)
+            return false;
+
+        if (screenPos.y > camera.pixelHeight - camera.pixelHeight * reduction)
+            return false;
+
+        return true;
     }
 
 	void FixedUpdate () {
+        bool zoomOut = false;
+        bool zoomIn = true;
+
+        for (int i = 0; i < targets.Length; i++){
+            if (!inScreen(targets[i].position, zoomOutZone))
+                zoomOut = true;
+
+            if (!inScreen(targets[i].position, zoomInZone))
+                zoomIn = false;
+        }
+
+        if (zoomOut)
+            height += zoomSpeed;
+
+        if (zoomIn)
+            height -= zoomSpeed;
+
         Vector3 average = AverageTarget();
-        
-        Vector3 movement = RelativeRotation(average);
 
-        movement = Vector3.Lerp(transform.position, movement, ease);
+        Vector3 newPos = RelativeRotation(average);    
 
-        transform.position = movement;
+        newPos = Vector3.Lerp(transform.position, newPos, ease);
+
+        transform.position = newPos;
 
         transform.LookAt(average);
 	}
